7451e1198e5d1e880b0231197662f363ab496167
张 <2573522468@qq.com>
2022-05-12 14:53:21 +0800

    合作伙伴修改

--- meinian_common/src/main/java/com/chd/util/testGit.java
+++ meinian_common/src/main/java/com/chd/util/testGit.java
@@ -7,6 +7,9 @@ public class testGit {
         System.out.println("testCommit");
         System.out.println("testCommit");
         System.out.println("testCommit");
-        
+         System.out.println("testCommit");
+        System.out.println("testCommit");
+        System.out.println("testCommit");
+
     }
 }
