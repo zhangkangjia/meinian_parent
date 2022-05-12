71d6da13f7d056a375b99e67fbb24082aa632437
张 <2573522468@qq.com>
2022-05-12 13:30:03 +0800

    testGit提交DEV

--- meinian_common/src/main/java/com/chd/util/testGit.java
+++ meinian_common/src/main/java/com/chd/util/testGit.java
@@ -4,5 +4,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("111111111111111111111Master");
         System.out.println("111111111111111111111DEV1");
+        System.out.println("testCommit");
     }
 }
