68759979f1e8c5c13da6577b8a76418348ff403e
张 <2573522468@qq.com>
2022-05-12 12:11:19 +0800

    testGit提交DEV

--- meinian_common/src/main/java/com/chd/util/testGit.java
+++ meinian_common/src/main/java/com/chd/util/testGit.java
@@ -2,6 +2,6 @@ package com.chd.util;
 
 public class testGit {
     public static void main(String[] args) {
-        System.out.println("111111111111111111111");
+        System.out.println("111111111111111111111DEV");
     }
 }
